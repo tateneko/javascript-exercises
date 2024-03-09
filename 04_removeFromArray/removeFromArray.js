4cdf415dc012d43309ae5346e3fc1e7127b87c62
felice <dellagattafelice@gmail.com>
2024-03-09 15:23:06 +0000

    Comple removeFromArray exercise. All tests ok.
    My solution pretty different from proposed solution but still working.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,24 +1,21 @@
 const removeFromArray = function() {
 
-    console.log(arguments)
-
     theArray = arguments[0];
     toRemove = Array.from(arguments).slice(1);
 
     for (i = toRemove.length - 1; i > -1; i--) {
+        
+        while (theArray.indexOf(toRemove[i]) != -1) {
 
         theArray.splice(theArray.indexOf(toRemove[i]), 1);
+
+        }
 
     }
 
     return theArray
 
 };
-
-
-
-removeFromArray([1,2,3,4,5,6],2,3)cd -4 
-
 
 
 // Do not edit below this line
