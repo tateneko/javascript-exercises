2d50c08ddda8cc32e6e907a548161db0e335d443
felice <dellagattafelice@gmail.com>
2024-03-09 15:07:45 +0000

    The code now passess the first 2 tets.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
 const removeFromArray = function() {
 
+    console.log(arguments)
+
+    theArray = arguments[0];
+    toRemove = Array.from(arguments).slice(1);
+
+    for (i = toRemove.length - 1; i > -1; i--) {
+
+        theArray.splice(theArray.indexOf(toRemove[i]), 1);
+
+    }
+
+    return theArray
+
 };
+
+
+
+removeFromArray([1,2,3,4,5,6],2,3)cd -4 
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
