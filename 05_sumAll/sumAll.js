592ead754c50aa8be872eaf78e07e273bc768d51
felice <dellagattafelice@gmail.com>
2024-03-10 18:40:24 +0000

    complete sumAll exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+
+    sortedArguments = [...args].sort(function(a, b){return a-b});
+
+    let sum = 0
+
+    if (arguments[0] > 0 && 
+        arguments[1] > 0 &&
+        Number.isInteger(arguments[0]) &&
+        Number.isInteger(arguments[1])) {
+
+        for (let i = sortedArguments[0]; i <= sortedArguments[1]; ++i) {
+            sum += i
+        }
+    
+    } else {
+
+        return "ERROR"
+
+    }
+
+    return sum
 
 };
 
