cd619a8ce5dbe3b8036aebd1f79d7a66044dd169
tateneko <dellagattafelice@gmail.com>
2024-03-22 19:36:10 +0000

    tempConversion exercise complete.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,11 @@
-const convertToCelsius = function() {
-
-test
-
+const convertToCelsius = function(temp) {
+  const celsius = Math.round(((temp - 32) * 5/9) * 10 ) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahrenheit = Math.round((temp * 9/5 + 32) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
